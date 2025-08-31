22eefe16caa57923d46294fd27c8077bb8bcf75c
lainyxx <straightening3@gmail.com>
2025-09-01 08:07:54 +0900

    入力フォームの追加、入力内容のストレージ保存

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,19 +1,40 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar,IonInput, IonButtons, IonMenuButton, IonTitle, IonContent, IonItem, IonLabel, IonButton, IonList } from '@ionic/angular/standalone';
+import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-folder',
   templateUrl: './folder.page.html',
   styleUrls: ['./folder.page.scss'],
-  imports: [IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonItem, IonInput, IonLabel, IonButton, IonList, NgFor, FormsModule],
 })
 export class FolderPage implements OnInit {
-  public folder!: string;
+  title = "タスク登録";
+  tasks: { name: string }[] = [
+    { name: 'タスク1' },
+    { name: 'タスク2' },
+  ];
+  task: string;
   private activatedRoute = inject(ActivatedRoute);
   constructor() {}
 
+  ionViewWillEnter() {
+    if ('tasks' in localStorage) {
+      this.tasks = JSON.parse(localStorage.tasks);
+    } 
+  }
+
+  addTask() {
+    this.tasks.push({
+      name: this.task
+    });
+    localStorage.tasks = JSON.stringify(this.tasks);
+    this.task = '';
+  }
+
   ngOnInit() {
-    this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+    // this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
   }
 }
